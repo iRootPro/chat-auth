85ffddc7e0151a58dfefb705f4828c188a3271af
Alexander Neupokoev <admin@armavir.ru>
2025-02-07 08:47:09 +0300

    up grpc server

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	desc "github.com/irootpro/chat-auth/pkg/servers/grpc/user_v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const grpcPort = 3010
+
+type server struct {
+	desc.UnimplementedUserV1Server
+}
+
+func (s server) Create(ctx context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	fmt.Printf("email: %s, name: %s", req.Email, req.Name)
+	return &desc.CreateResponse{}, nil
+}
 
 func main() {
-	fmt.Println("Chat AUTH")
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+
+	desc.RegisterUserV1Server(s, &server{})
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", grpcPort))
+	if err != nil {
+		log.Fatalf("failed to listen %v", err)
+	}
+
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve grpc server %v", err)
+	}
 }
